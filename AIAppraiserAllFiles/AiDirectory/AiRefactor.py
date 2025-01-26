3215d91afb3bcb593f35202cef43b6fdf81e0965
Manterimon <8982243917k@gmail.com>
2025-01-26 21:30:35 +0300

    На данном этапе релиазованно две базы данных, которые были спаршаны с сайта funpey и разделены собственно на тестовую и обучающую БД. Создан функция по составлению 2 графиков - гистограммы и Q-Q plot которые показывают насколько нормализованны данные. Так же нейросеть более мение работает

--- AIAppraiserAllFiles/AiDirectory/AiRefactor.py
+++ AIAppraiserAllFiles/AiDirectory/AiRefactor.py
@@ -1,55 +1,76 @@
 import pandas as pd
-from keras.src.layers import Dense
+from keras.src.layers import Dense, Input
 from keras.src.models import Sequential
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from keras.src.regularizers import L2
+from keras.src.callbacks import EarlyStopping
 from tensorflow.python.framework.test_ops import kernel_label_required
 
 
 class AI_Creait():
     def __init__(self):
-        self.df_training = pd.read_csv('/AIAppraiserAllFiles/ExecutableDirectory/first_DFFP.csv')
-        self.df_check = pd.read_csv('/AIAppraiserAllFiles/ExecutableDirectory/last_DFFP.csv')
+        self.df_training = pd.read_csv('/home/I/SchoolProject/AIAppraiserAllFiles/ExecutableDirectory/first_DFFP.csv')
+        self.df_check = pd.read_csv('/home/I/SchoolProject/AIAppraiserAllFiles/ExecutableDirectory/last_DFFP.csv')
 
-    def df_division(self, df, test_size=0.2, random_state=42):
+    def df_division(self, df):
         X = df[['Cup', 'Hero']]
         y = df['Price']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-        return X_train, X_test, y_train, y_test
+        return X, y
 
-    def train_model(self):
-        # Разделение данных
-        X_train, X_test, y_train, y_test = self.df_division(self.df_training)
+    def train_model_with_kfold(self, n_splits=16):
+        print("Starting K-Fold training...")
+        X, y = self.df_division(self.df_training)
+        kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+        fold_losses = []
 
-        # Нормализация данных
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
+        for fold, (train_index, test_index) in enumerate(kf.split(X), 1):
+            print(f"Training fold {fold}...")
+            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        # Создание модели
-        model = Sequential([
-            Dense(64, activation='relu', input_shape=(X_train_scaled.shape[1],), kernel_regularizer=L2(0.01)),
-            Dense(32, activation='relu', kernel_regularizer=L2(0.01)),
-            Dense(1)
-        ])
+            # Нормализация данных
+            self.scaler = StandardScaler()
+            X_train_scaled = self.scaler.fit_transform(X_train)
+            X_test_scaled = self.scaler.transform(X_test)
 
-        # Компиляция модели
-        model.compile(optimizer='adam', loss='mean_squared_error')
+            # Создание модели
+            self.model = Sequential([
+                Input(shape=(X_train_scaled.shape[1],)),
+                Dense(64, activation='relu', kernel_regularizer=L2(0.01)),
+                Dense(32, activation='relu', kernel_regularizer=L2(0.01)),
+                Dense(16, activation='relu', kernel_regularizer=L2(0.01)),
+                Dense(8, activation='relu', kernel_regularizer=L2(0.01)),
+                Dense(4, activation='relu', kernel_regularizer=L2(0.01)),
+                Dense(1)
+            ])
 
-        # Обучение модели
-        model.fit(X_train_scaled, y_train, epochs=100, batch_size=10, verbose=1)
+            # Компиляция модели
+            self.model.compile(optimizer='adam', loss='mean_squared_error')
+            early_stopping = EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
+            # Обучение модели
+            self.history = self.model.fit(X_train_scaled, y_train, epochs=2550, batch_size=32, verbose=1, callbacks=[early_stopping])
 
-        # Оценка модели
-        loss = model.evaluate(X_test_scaled, y_test, verbose=0)
-        print(f'Loss: {loss}')
+            # Оценка модели
+            loss = self.model.evaluate(X_test_scaled, y_test, verbose=0)
+            print(f"Fold {fold} loss: {loss}")
+            fold_losses.append(loss)
 
-        # Сохранение модели и скейлера для дальнейшего использования
-        self.model = model
-        self.scaler = scaler
+
+        average_loss = sum(fold_losses) / len(fold_losses)
+        print(f'Average Loss across {n_splits} folds: {average_loss}')
+
+        self.model.save('AIAppraiser.keras')
+
+        plt.plot(self.history.history['loss'])
+        plt.title('Model Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.savefig(f'{n_splits}-model_loss.png')
+        plt.close()
 
     def predict(self, cup, hero):
         # Пример предсказания
@@ -61,18 +82,21 @@ class AI_Creait():
     def Q_Q_plot(self, data):#провека нормального расспределения данных
         # Гистограмма
         plt.hist(data, bins=10, alpha=0.6, color='g')
-        plt.title('Histogram')
+        plt.title(f'{data.name}-Histogram')
+        plt.savefig(f'{data.name}-Histogram.png')
         plt.show()
+        plt.close()
 
         # Q-Q Plot
         stats.probplot(data, dist="norm", plot=plt)
-        plt.title('Q-Q Plot')
+        plt.title(f'{data.name}-Q-Q Plot')
+        plt.savefig(f'{data.name}-Q_Q_pot.png')
         plt.show()
+        plt.close()
 
 if __name__ == '__main__':
     model = AI_Creait()
-    model.Q_Q_plot(model.df_training['Hero'])
-    model.train_model()
+    model.train_model_with_kfold()
 
     # Итерируемся по каждой строке в DataFrame df_check
     for index, row in model.df_check.iterrows():
@@ -80,7 +104,6 @@ if __name__ == '__main__':
         hero = row['Hero']
         prediction = model.predict(cup, hero)
         print(f'Предсказание для Cup: {cup}, Hero: {hero} - {prediction}')
-
 
 
 
